ffe18c88881cb603fed0df2c3c73f8b7d74ae274
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-10-16 23:25:21 +0500

    ввод отношения критериев в матрицу

--- Фт-210008_Терентьева_анализ_иерархий.py
+++ Фт-210008_Терентьева_анализ_иерархий.py
@@ -1,1 +1,13 @@
 def inMatrix(mat, n):
+  for i in range(n):
+        for j in range(n):
+            if (i == j):
+                mat[i][j] = 1 
+            if (i < j):
+                while mat[i][j] == 0: 
+                    temp = input(f"Введите отношение критерия {i+1} к критерию {j+1} ")
+                    temp = errorCheck(temp)
+                    if  (temp > 9) or (temp < 1): 
+                        print('Отношение должно быть целым положительным числом от 1 до 9')
+                    else:
+                        mat[i][j] = temp
